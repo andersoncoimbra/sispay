114c2272335bbae6e4ef8b6e31e3d738a42b1bc2
Anderson Coimbra <andersonmegax@hotmail.com>
2022-08-07 21:53:13 -0300

    arquivos para tradução em pt-br

---
+++ resources/lang/pt-br/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Linhas de linguagem de autenticação
+    |--------------------------------------------------------------------------
+    |
+    | As seguintes linhas de idioma são usadas durante a autenticação para várias
+    | mensagens que precisamos exibir ao usuário. Você é livre para modificar essas
+    | linhas de idioma de acordo com as exigências do seu aplicativo.
+    |
+    */
+
+    'failed' => 'Essas credenciais não foram encontradas em nossos registros.',
+    'password' => 'A senha informada está incorreta.',
+    'throttle' => 'Muitas tentativas de login. Tente novamente em :seconds segundos.',
+
+];
